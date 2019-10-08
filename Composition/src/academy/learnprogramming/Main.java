16888c5b5ca640a7a40ad3ca8a6d01d3543c257c
Daniel Flynn <dfflynn@uk-c02z42c7lvdl.uk.deloitte.com>
2019-10-08 13:51:23 +0100

    Completed Challenge

--- Composition/src/academy/learnprogramming/Main.java
+++ Composition/src/academy/learnprogramming/Main.java
@@ -13,5 +13,34 @@ public class Main {
 	    PC thePC = new PC(theCase, theMonitor, theMotherboard);
 
 	   thePC.powerUp();
+
+	   	// Composition Challenge
+		// Create a single room of a house using composition.
+		// Think about the things that should be included in the room.
+		// Maybe physical parts of the house but furniture as well.
+		// At least one method to access an object via a getter and
+		// then that objects public method as you saw previously
+		// then at at least one method to hide the object e.g. not using a getter
+		// but to access the object use in composition within the main class
+
+
+		Wall wall1 = new Wall("West");
+		Wall wall2 = new Wall("East");
+		Wall wall3 = new Wall("South");
+		Wall wall4 = new Wall("North");
+
+		Ceiling ceiling = new Ceiling(12, 55);
+
+		Bed bed = new Bed("Posh", 4, 6, 2, 1);
+
+		Lamp lamp = new Lamp("Posh", false, 100);
+
+		Bedroom bedroom = new Bedroom("Tims", wall1, wall2, wall3,wall4,ceiling,bed,lamp);
+
+		bedroom.makeBed();
+
+		bedroom.getLamp().turnOn();
+
+
     }
 }
